b21f8fa51e3069bf22c7583ea140789c0ae2d72a
UgoMarin <78205951+UgoMarin@users.noreply.github.com>
2023-03-13 20:50:50 +0100

    modif db

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 const ADODB = require('node-adodb');
-const connect = ADODB.open('Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\aocol\OneDrive\Documents\IMT\Entreprise\Test\Affichage ELP.accdb;');
+const connect = ADODB.open('Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C://Users//aocol//OneDrive//Documents//IMT//Entreprise//Test//Affichage ELP.accdb;');
 exports.connection = connect
 
